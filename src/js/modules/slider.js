0049fa10eda18e0caa1600f8091617316db75615
melnik <melnik@bifit.com>
2022-08-10 12:58:23 +0300

    Slider. Hero block. Finishes

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -1,7 +1,16 @@
 // Подключение свайпера
-import Swiper, { Navigation, Scrollbar } from 'swiper';
-Swiper.use([Navigation, Scrollbar]);
+import Swiper, { Navigation, Scrollbar, Pagination } from 'swiper';
+Swiper.use([Navigation, Scrollbar, Pagination]);
 
-const swiper = new Swiper(".hero__swiper", {
-
+new Swiper(".hero__swiper", {
+  pagination: {
+    el: '.hero__pagination',
+    clickable: true,
+    bulletClass: 'hero__pagination-bullet',
+    bulletActiveClass: "hero__pagination-bullet--active",
+    renderBullet: function (index, className) {
+      const number = index < 9 ? `0${index + 1}` : index + 1;
+      return `<span class="${className}">${number}</span>`
+    },
+  }
 });
